c2c89ec3bc6a50afa92e1435c02eee5f7b41f38d
Siaw Henry Nartey <henrysiaw159@gmail.com>
2023-12-04 16:23:41 +0000

    CHORE: views partials components

--- views/departments/index.view.php
+++ views/departments/index.view.php
@@ -1,6 +1,9 @@
 <?php require base_path('views/partials/head.php') ?>
+    <div class="min-h-full">
+    <div class="sticky top-0 z-10">
 <?php require base_path('views/partials/nav.php') ?>
 <?php require base_path('views/partials/banner.php') ?>
+    </div>
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -115,5 +118,6 @@
 
         </div>
     </main>
+    </div>
 
 <?php require base_path('views/partials/footer.php') ?>
--- views/partials/banner.php
+++ views/partials/banner.php
@@ -3,4 +3,3 @@
         <h1 class="text-3xl font-bold tracking-tight text-gray-900"> <?= $heading ?? '' ?></h1>
     </div>
 </header>
-</div>
